e711f0b0a835d329eca5cc6f367f31b962ca6f1b
Suvin Gurung <suvin.gurung@yahoo.com>
2016-12-25 04:15:04 +0545

    v0.6.1 - new_session function

--- states/match.js
+++ states/match.js
@@ -653,6 +653,15 @@ game.match.prototype = {
         this.overs_left_text.setText('BALLS LEFT - ' + this.overs_left);  
     },
 
+    new_session: function () {
+        
+
+        this.stadium.visible = false;
+        this.scoreboard.destroy();
+        this.run_text.destroy();
+        this.hit_ball.destroy();
+
+    },
 
     }
 
